b1c975e5b0ad13526d1ddfdba5f2283089038c27
geo derek <geoderek@HMCSE-JMORGAN-FC.argo.uwf.edu>
2019-01-26 15:55:14 -0600

    create_sample_stations

---
+++ sql/create_sample_stations.sql
@@ -0,0 +1,5 @@
+--TODO: 
+-- create sample stations for escambia and bayou texar
+-- populate/create point geom from lat/lons 
+-- make sure it is in 4326
+--See https://postgis.net/docs/ST_MakePoint.html for how to make a geom point from a lat/lon
