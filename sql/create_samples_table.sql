d5c0f6f2949410dbd3520ef621afdd2636223c8b
geo derek <geoderek@HMCSE-JMORGAN-FC.argo.uwf.edu>
2019-02-02 13:49:11 -0600

    test
    test

--- sql/create_samples_table.sql
+++ sql/create_samples_table.sql
@@ -1,11 +1,3 @@
-SELECT version();
-SELECT PostGIS_full_version();
-
-
-CREATE DATABASE water_quality;
-CREATE EXTENSION postgis;
-CREATE SCHEMA wq;
-
 CREATE TABLE wq.samples (
   site varchar(20),
   value numeric
