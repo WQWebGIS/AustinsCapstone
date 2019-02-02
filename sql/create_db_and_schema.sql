20d02fb09159c785718f37adf5b39fdf2defdc91
jderekito <johnderekmorgan@hotmail.com>
2019-02-02 13:13:23 -0600

    sql
    sql

--- sql/create_db_and_schema.sql
+++ sql/create_db_and_schema.sql
@@ -1,12 +1,10 @@
 SELECT version();
-SELECT PostGIS_full_version();
+
 
 
 CREATE DATABASE water_quality;
+--Now switch to new DB
 CREATE EXTENSION postgis;
+SELECT PostGIS_full_version();
 CREATE SCHEMA wq;
 
-CREATE TABLE wq.samples (
-  site varchar(20),
-  value numeric
-);
