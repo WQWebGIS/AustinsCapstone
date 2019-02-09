364d1ffbd637d441ae205b1347d66bdd98b655b2
geo derek <geoderek@HMCSE-JMORGAN-FC.argo.uwf.edu>
2019-02-09 10:55:04 -0600

    added lat lon functionality
    added lat lon functionality

--- sql/create_sample_stations.sql
+++ sql/create_sample_stations.sql
@@ -4,7 +4,7 @@
 -- make sure it is in 4326
 --See https://postgis.net/docs/ST_MakePoint.html for how to make a geom point from a lat/lon
 
-DROP TABLE wq.sample_stations
+DROP TABLE wq.sample_stations;
 
 CREATE TABLE wq.sample_stations (
   "gid" integer,
@@ -15,6 +15,8 @@ CREATE TABLE wq.sample_stations (
   "county" varchar(30),
   "waterbody_id" varchar(30),
   "primary_type" varchar(30),
+  "latitude" float, 
+  "longitude" float,
   "datum" varchar(30),
   "huc" integer
 );
@@ -26,7 +28,8 @@ CREATE TABLE wq.sample_stations (
 SELECT AddGeometryColumn ('wq','sample_stations','geom',4326,'POINT',2);
 
 
-INSERT INTO wq.sample_stations (gid, org_id, org_name, station_id, station_name, county, waterbody_id, primary_type, datum, huc)
-    VALUES (1, '21FLBFA', 'FL Dept of Environmental Protection , Bream Fisherman Assoc.', '33020HF1', 'BAYOU TEXAR AT 12TH AVE BRIDGE', 'ESCAMBIA', '738', 'River\/Stream', 'NAD83', '3140105');
+INSERT INTO wq.sample_stations (gid, org_id, org_name, station_id, station_name, county, waterbody_id, primary_type, latitude, longitude, datum, huc)
+    VALUES (1, '21FLBFA', 'FL Dept of Environmental Protection , Bream Fisherman Assoc.', '33020HF1', 'BAYOU TEXAR AT 12TH AVE BRIDGE', 'ESCAMBIA', '738', 'River\/Stream', '30.46002', '-87.208', 'NAD83', '3140105');
 
-
+UPDATE wq.sample_stations
+SET geom=st_SetSrid(st_MakePoint(longitude, latitude), 4326)
