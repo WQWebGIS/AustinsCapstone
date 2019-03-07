eef12acfd9366254ab8630ba0296be46af659d0e
geo derek <geoderek@HMCSE-JMORGAN-FC.argo.uwf.edu>
2019-03-07 16:43:23 -0600

    geojson items
    geojson items

--- pgsql_mod.js
+++ pgsql_mod.js
@@ -1,5 +1,4 @@
 // https://www.sitepoint.com/understanding-module-exports-exports-node-js/
-// 
 
 const { Client } = require('pg')
 const client = new Client({
@@ -11,18 +10,25 @@ const client = new Client({
 })
 
 module.exports = {
-    test: function() {
-		//keeping a variable passed from parent module
-        console.log('var is', this.fileRows);
+	getSampleStations: function () {
+		client.connect();
+		// callback
+		client.query('SELECT * FROM wq.get_wb_json(2);', (err, res) => {
+			if (err) {
+				console.log(err.stack)
+			} else {
+				console.log(res.rows)
+			}
+		});
 	},
 	getWaterBodies: function () {
 		client.connect();
 		// callback
 		client.query('SELECT NOW() as now', (err, res) => {
 			if (err) {
-			console.log(err.stack)
+				console.log(err.stack)
 			} else {
-			console.log(res.rows[0])
+				console.log(res.rows[0])
 			}
 		});
 	},
@@ -31,7 +37,6 @@ module.exports = {
 		//We want to insert an array of arrays (result of CSV pop var)
 		//Adapted from https://stackoverflow.com/questions/8899802/how-do-i-do-a-bulk-insert-in-mysql-using-node-js
 		//and https://stackoverflow.com/questions/46681278/how-to-insert-multiple-rows-using-node-postgres
-		
 		for(var i = 1; i < this.fileRows.length; i++){
 			console.log(this.fileRows[i][0]+' '+this.fileRows[i][1]);
 			client.query('INSERT INTO wq.samples(site, value) VALUES ($1, $2) RETURNING *', [this.fileRows[i][0], this.fileRows[i][1]], (err, res) => {
--- sql/create_samples_table.sql
+++ sql/create_samples_table.sql
@@ -52,3 +52,53 @@ CREATE TABLE wq.do (
   "procedure_source" varchar(100),
   "matrix" varchar(100)
 );
+
+CREATE TABLE wq.nitromixed (
+  "org_id" varchar(50),
+  "station_id" varchar(50),
+  "station_name" varchar(100),
+  "characteristic_id" integer,
+  "characteristic" varchar(100),
+  "result_value" float,
+  "result_unit" varchar(10),
+  "date_tested" varchar(100),
+  "replicate_num" varchar(100),
+  "activity_medium" varchar(100),
+  "activity_type" varchar(100),
+  "activity_category" varchar(100),
+  "rel_depth" varchar(100),
+  "activity_depth" varchar(100),
+  "depth_unit" varchar(10),
+  "sample_fraction" varchar(100),
+  "value_type" varchar(100),
+  "result_comment" varchar(100),
+  "value_qualifier" varchar(100),
+  "analytical_procedure" varchar(100),
+  "procedure_source" varchar(100),
+  "matrix" varchar(100)
+);
+
+CREATE TABLE wq.dos (
+  "org_id" varchar(50),
+  "station_id" varchar(50),
+  "station_name" varchar(100),
+  "characteristic_id" integer,
+  "characteristic" varchar(100),
+  "result_value" float,
+  "result_unit" varchar(10),
+  "date_tested" varchar(100),
+  "replicate_num" varchar(100),
+  "activity_medium" varchar(100),
+  "activity_type" varchar(100),
+  "activity_category" varchar(100),
+  "rel_depth" varchar(100),
+  "activity_depth" varchar(100),
+  "depth_unit" varchar(10),
+  "sample_fraction" varchar(100),
+  "value_type" varchar(100),
+  "result_comment" varchar(100),
+  "value_qualifier" varchar(100),
+  "analytical_procedure" varchar(100),
+  "procedure_source" varchar(100),
+  "matrix" varchar(100)
+);
